53973dd1358a26a21526e3e93368d8eac670dbfb
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2014-03-04 08:55:10 -0300

    Avoid annoying warning from I18n

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,5 +13,8 @@ require "rails/generators/test_case"
 $:.unshift File.expand_path("../../lib", __FILE__)
 require "markerb"
 
+# Avoid annoying warning from I18n.
+I18n.enforce_available_locales = false
+
 ActionMailer::Base.delivery_method = :test
 ActionMailer::Base.perform_deliveries = true
